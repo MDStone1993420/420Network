719a44c2df1e24ffb615d35152195c12ee238426
abvhiael stuart <abvhiael@420integrated.com>
2020-10-27 00:16:20 -0300

    Update AddPost.js

--- src/components/forms/AddPost.js
+++ src/components/forms/AddPost.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react'
-import WhaleNetwork from '../../../build/contracts/WhaleNetwork.json'
-import WhaleRewards from '../../../build/contracts/WhaleRewards.json'
+import CannasseurNetwork from '../../../build/contracts/CannasseurNetwork.json'
+import CannasseurRewards from '../../../build/contracts/CannasseurRewards.json'
 import getWeb3 from '../../utils/getWeb3'
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ReactDOM from 'react-dom'
-import {keystore, txutils} from 'eth-lightwallet'
+import {keystore, txutils} from 'fourtwenty-lightwallet'
 import tx from 'ethereumjs-tx'
 import Header from '../header.js'
 import Paper from 'material-ui/Paper';
@@ -34,7 +34,7 @@ class AddPost extends Component {
     super(props)
 
     this.state = {
-      whale: '',
+      cannasseur: '',
       address: '',
       post: '',
       privateKey: '',
@@ -45,7 +45,7 @@ class AddPost extends Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handlePostChange = this.handlePostChange.bind(this);
-    this.handleWhaleChange = this.handleWhaleChange.bind(this);
+    this.handleCannasseurChange = this.handleCannasseurChange.bind(this);
   }
 
   componentWillMount() {
@@ -73,40 +73,40 @@ class AddPost extends Component {
     this.setState({post: event.target.value});
   }
 
-  handleWhaleChange(event) {
-    this.setState({whale: event.target.value});
+  handleCannasseurChange(event) {
+    this.setState({cannasseur: event.target.value});
   }
   // on form submit this is the action called
   handleSubmit(event) {
     event.preventDefault();
     event.preventDefault();
     const contract = require('truffle-contract')
-    const whaleNetwork = contract(WhaleNetwork)
-    const whaleRewards = contract(WhaleRewards)
-    whaleRewards.setProvider(this.state.web3.currentProvider)
-    whaleNetwork.setProvider(this.state.web3.currentProvider)
+    const cannasseurNetwork = contract(CannasseurNetwork)
+    const cannasseurRewards = contract(CannasseurRewards)
+    cannasseurRewards.setProvider(this.state.web3.currentProvider)
+    cannasseurNetwork.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var whaleRewardsInstance
-    var whaleNetworkInstance
+    var cannasseurRewardsInstance
+    var cannasseurNetworkInstance
 
     // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      whaleRewards.deployed().then((instance) => {
-        whaleRewardsInstance = instance
+    this.state.web3.fourtwenty.getAccounts((error, accounts) => {
+      cannasseurRewards.deployed().then((instance) => {
+        cannasseurRewardsInstance = instance
 
         // Stores a given value, 5 by default.
-        return whaleRewardsInstance.getNetworkAddress.call({from: accounts[0]})
+        return cannasseurRewardsInstance.getNetworkAddress.call({from: accounts[0]})
       }).then((result) => {
         // Get the value from the contract to prove it worked.
         console.log(result)
-        whaleNetwork.at(result).then((whaleNet) => {
-            whaleNetworkInstance = whaleNet;
+        cannasseurNetwork.at(result).then((cannasseurNet) => {
+            cannasseurNetworkInstance = cannasseurNet;
 
         var txOptions = {
-          nonce: this.state.web3.toHex(this.state.web3.eth.getTransactionCount(this.state.address)),
-          gasLimit: this.state.web3.toHex(2000000),
-          gasPrice: this.state.web3.toHex(20000000000),
+          nonce: this.state.web3.toHex(this.state.web3.fourtwenty.getTransactionCount(this.state.address)),
+          smokeLimit: this.state.web3.toHex(2000000),
+          smokePrice: this.state.web3.toHex(20000000000),
           to: result,
           from: this.state.address
         }
@@ -121,12 +121,12 @@ class AddPost extends Component {
         console.log(3)
         var serializedTx = transaction.serialize().toString('hex');
         console.log(serializedTx)
-        this.state.web3.eth.sendRawTransaction('0x' + serializedTx, function(err, result) {
+        this.state.web3.fourtwenty.sendRawTransaction('0x' + serializedTx, function(err, result) {
           if (err) {
             console.log(err);
           } else {
             console.log(result);
-            whaleNetworkInstance.Posted(function(error, data) {
+            cannasseurNetworkInstance.Posted(function(error, data) {
               if (error) {
                 console.log(error);
               } else {
@@ -159,7 +159,7 @@ class AddPost extends Component {
 
               </Grid>
               <Grid item xs={12} >
-                <TextField fullWidth label="Enter Whale Address" value={this.state.whale} onChange={this.handleWhaleChange} />
+                <TextField fullWidth label="Enter 420coin Address" value={this.state.cannasseur} onChange={this.handleCannasseurChange} />
 
                 </Grid>
               <Grid item xs={12}>
